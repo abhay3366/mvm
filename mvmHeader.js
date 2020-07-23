104a7ad96f2ca43608e578d9d45a4484a536cb0b
thapatechnical <tabhaykant1@gmail.com>
2020-07-23 02:07:39 -0700

    First commit

---
+++ mvmHeader.js
@@ -0,0 +1,23 @@
+function myfunction(){
+   document.getElementById(admission).style.position="absolute";
+ }
+ function myfunction1(){
+   document.getElementById(academic).style.position="absolute";
+ }
+ function myfunction2(){
+  document.getElementById(circular).style.position="absolute";
+}
+function myfunction3(){
+  document.getElementById(facilities).style.position="absolute";
+}
+function myfunction5(){
+  document.getElementById(corner).style.position="absolute";
+}
+function myfunction6(){
+  document.getElementById(career).style.position="absolute";
+  document.getElementById(career).style.minWidth="350px";
+}       
+function myfunction7(){
+  document.getElementById(contact).style.position="absolute";
+  document.getElementById(contact).style.minWidth="350px";
+}
